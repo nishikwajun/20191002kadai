c8ca20d35e168e601e004e5fa25ee1e43001c80d
nishikwajun <junpei844sw@gmail.com>
2019-11-27 04:58:48 +0000

    bbbb

---
+++ kadai-6/index2.js
@@ -0,0 +1,17 @@
+const str1 = "abc"
+const str2 = "'abc'"
+const str3 = '"abc"'
+const str4 = '{"a": undefined}'
+const str5 = '{"a": 123}'
+const str6 = '{a: 123}'
+
+
+try {
+    console.log(JSON.parse(str1))
+} catch (error) {
+    console.log(error)
+    // abc という文字列は正しいJSONではないから。
+    //正しくは"abc"
+}
+
+console.log(JSON.stringify(str3))
---
+++ kadai-6/test.js
@@ -0,0 +1,9 @@
+const cat = {
+    species: "cat",
+    name: "Tama",
+    age: 2,
+    sex: "male"
+};
+
+const jsonstr = JSON.stringify(cat);
+console.log(jsonstr);
